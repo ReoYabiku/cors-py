7c3c0b09b4b90d716c3be4e4dd861557b5eec2db
ReoYabiku <alcremikyu.z@gmail.com>
2026-05-02 00:09:58 +0900

    uv format

--- main.py
+++ main.py
@@ -7,29 +7,37 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ['http://localhost:8080']
+origins = ["http://localhost:8080"]
 # origins = []
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods = ['*'],
-    allow_headers = ['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+
 @app.get("/")
-async def root(dammy_id: int = None, cors_app_id: Annotated[str | None, Cookie()] = None):
+async def root(
+    dammy_id: int = None, cors_app_id: Annotated[str | None, Cookie()] = None
+):
     return {
         "message": "問題なし！",
         "dammy_id": dammy_id,
         "cors_app_id": cors_app_id,
     }
 
+
 @app.get("/set-cookie")
-async def set_cookie(response: Response, cors_app_id: Annotated[str | None, Cookie()] = None):
+async def set_cookie(
+    response: Response, cors_app_id: Annotated[str | None, Cookie()] = None
+):
     if cors_app_id is None or len(cors_app_id) == 0:
-        response.set_cookie(key="cors_app_id", value=''.join(random.choices(string.ascii_letters + string.digits, k=12)), httponly=True)
-    
-    return {
-        "message": "success"
-    }
+        response.set_cookie(
+            key="cors_app_id",
+            value="".join(random.choices(string.ascii_letters + string.digits, k=12)),
+            httponly=True,
+        )
+
+    return {"message": "success"}
